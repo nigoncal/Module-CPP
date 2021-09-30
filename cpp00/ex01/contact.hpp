beea4e3fb36c1452bc8c517c5f4cc042d6877845
Nicolas Goncalves <nigoncal@z4r2p7.42lyon.fr>
2021-09-30 16:52:03 +0200

    v2, not fuctional

--- cpp00/ex01/contact.hpp
+++ cpp00/ex01/contact.hpp
@@ -4,24 +4,22 @@
 class contact
 {
 	public :
+		std::string show_contact() const;
+		void print_attribute() const;
+		
 	
-	int get_first_name() const; 
-	int get_last_name() const;
-	int get_nickname() const;
-	int get_phone_number() const; 
-	int get_darknest_secret() const;
-	
-	std::string set_first_name();
-	std::string set_last_name();
-	std::string set_nickname();
-	std::string set_phone_number();
-	std::string set_darknest_secret();
+		std::string get_first_name() const; 
+		std::string get_last_name() const;
+		std::string get_nickname() const;
+		std::string get_phone_number() const;
+		std::string get_darkest_secret() const;
+		void set_attribute();
 
-	private:
+	private :
 
-	std::string first_name;
-	std::string	last_name;
-	std::string nickname;
-	std::string phone_number;
-	std::string darkest_secret;
+		std::string _first_name;
+		std::string	_last_name;
+		std::string _nickname;
+		std::string _phone_number;
+		std::string _darkest_secret;
 };
---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,79 @@
+#include "contact.hpp"
+#include <iomanip>
+
+std::string resize_string (std::string src)
+{
+	if(src.size() > 9)
+	{
+		src.resize(9);
+		src.append(".");
+	}
+	return(src);
+}
+
+void	function_search(contact *list, int i)
+{
+	int index;
+	std::string buffer; 
+	index = 0;
+			if(i == 0)
+				std::cout<<"no contact"<<std::endl;
+			else
+			{
+				std::cout<<"|"<<std::setw(10)<<"index";
+				std::cout<<"|"<<std::setw(10)<<"first_name";
+				std::cout<<"|"<<std::setw(10)<<"last_name";
+				std::cout<<"|"<<std::setw(10)<<"nick_name"<<"|"<<std::endl;
+				for(int j = 0; j != i; j++)
+				{
+					std::cout<<"|"<<std::setw(10)<<j;
+					std::cout<<"|"<<std::setw(10)<<resize_string(list[j].get_first_name());
+					std::cout<<"|"<<std::setw(10)<<resize_string(list[j].get_last_name());
+					std::cout<<"|"<<std::setw(10)<<resize_string(list[j].get_nickname())<<"|"<<std::endl;
+				}
+				std::cout<<std::endl<<"index : ";
+				std::cin.ignore();
+				std::getline(std::cin, buffer);
+				//std::cin>>buffer;
+				index = atoi(buffer.c_str());
+				if((index >= 0 && index <= 8) && index < i)
+					std::cout<<index;
+				else
+					std::cout<<"erreur index"<<std::endl;
+			}
+				//	list[index].print_attribute();
+}
+
+int main ()
+{
+	bool status = true;
+	std::string get_string;
+	contact list[8];
+	int i;
+	i = 0;
+
+	while(status)
+	{
+		std::cin>>get_string;
+		if(!get_string.compare("EXIT"))
+		{
+			std::cout<<"see you soon !"<<std::endl;
+			return(0);
+		}
+		if(!get_string.compare("ADD"))
+		{
+			if(i != 8)
+			{
+				list[i].set_attribute();
+				i++;
+			}
+			else
+				std::cout<<"contact full !"<<std::endl;
+		}
+		if(!get_string.compare("SEARCH"))
+		{
+			function_search(list, i );
+		}
+	}
+
+}
